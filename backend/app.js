0aed39933b60bf81a84cd094cba8cbdd0c2bd9ef
vipul kumar <bca23161@glbim.ac.in>
2024-12-25 12:57:04 +0530

    some changes

--- backend/app.js
+++ backend/app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 const cookieParser = require("cookie-parser");
 const cors = require("cors");
-const { connectDB } = require('./config/database');
+const { connectDB } = require("./src/config/database");
 app.use(
   cors({
     origin: "http://localhost:5173",
